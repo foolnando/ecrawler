8fdea77cb6e5012cf9aae531eb0ff3e1f1c2154c
foolnando <fernandoonv06@gmail.com>
2020-05-13 15:14:58 -0400

    modificações feitas no pos parse e teste no site da magalu

--- ecrawler/spiders/excommerce.py
+++ ecrawler/spiders/excommerce.py
@@ -12,14 +12,18 @@ import re
 
 class ExcommerceSpider(scrapy.Spider):
     name = 'excommerce'
-    allowed_domains = ['nerdstickers.com.br']
-    start_urls = ['https://www.nerdstickers.com.br/']
+    allowed_domains = ['magazineluiza.com.br']
+    start_urls = ['https://www.magazineluiza.com.br/']
     scoreUrl = []
     url = []
     keywords = ['entrar','cart','carrinho','login','logar',
     'faq','sac','faleconosco','fale-conosco','remacoes',
     'checkout','logout', 'checkin', 'minha-conta', 'conta', 
-    'account', 'External']
+    'account', 'External', 'blog', 'cartao']
+    entryUrl = 'https://www.magazineluiza.com.br/'
+
+
+
     def createMatrix(self,A,B):
         '''cria e preenche uma matriz de i linhas e j colunas'''
         matrix = []
@@ -151,62 +155,128 @@ class ExcommerceSpider(scrapy.Spider):
         comp = 'https://www.nerdstickers.com.br/'
         if not self.scoreUrl:
             print("link has not been given a value")
-        hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage ='ward')
-        y_hc=hc.fit_predict(self.scoreUrl)
-        y = np.array(self.scoreUrl)
-        plt.scatter(y[y_hc==0, 0], y[y_hc==0, 1], s=100, c='red', label ='Cluster 1')
-        plt.scatter(y[y_hc==1, 0], y[y_hc==1, 1], s=100, c='blue', label ='Cluster 2')
-        plt.scatter(y[y_hc==2, 0], y[y_hc==2, 1], s=100, c='green', label ='Cluster 3')
-        plt.scatter(y[y_hc==3, 0], y[y_hc==3, 1], s=100, c='cyan', label ='Cluster 4')
-        plt.scatter(y[y_hc==4, 0], y[y_hc==4, 1], s=100, c='magenta', label ='Cluster 5')
+        hc = AgglomerativeClustering(n_clusters = 4, affinity = 'euclidean', linkage ='ward')
+        x_hc=hc.fit_predict(self.scoreUrl)
+        x = np.array(self.scoreUrl)
+        plt.scatter(x[x_hc==0, 0], x[x_hc==0, 1], s=100, c='red', label ='Cluster 1')
+        plt.scatter(x[x_hc==1, 0], x[x_hc==1, 1], s=100, c='blue', label ='Cluster 2')
+        plt.scatter(x[x_hc==2, 0], x[x_hc==2, 1], s=100, c='green', label ='Cluster 3')
+        plt.scatter(x[x_hc==3, 0], x[x_hc==3, 1], s=100, c='cyan', label ='Cluster 4')
+        plt.scatter(x[x_hc==4, 0], x[x_hc==4, 1], s=100, c='magenta', label ='Cluster 5')
         plt.title('Clusters of Url Score of a site (Hierarchical Clustering Model)')
         plt.xlabel('Score do alinhamento')
         plt.ylabel('minimo radical comum')
         plt.show()
 
-        grtst=0
-        indxGrts=0
-        for i in range(0,4):
-            y0 = y[y_hc==i, 0], y[y_hc==i, 1]
-            if(len(y0[0])>grtst):
-                indxGrts=i
-                grtst = len(y0[0])
+        # grtst=0
+        # indxGrts=0
+        # for i in range(0,4):
+        #     x0 = x[x_hc==i, 0], x[x_hc==i, 1]
+        #     if(len(x0[0])>grtst):
+        #         indxGrts=i
+        #         grtst = len(x0[0])
                 
-        print(grtst, indxGrts)
-        print(y[y_hc==indxGrts, 0], y[y_hc==indxGrts, 1])
+        # print(grtst, indxGrts)
+        # print(len(x[x_hc==0, 0]))
+        # print(len(x[x_hc==1, 0]))
+        # print(len(x[x_hc==2, 0]))
+        # print(len(x[x_hc==3, 0]))
+        # print(len(x[x_hc==4, 0]))
         
-        compx = y[y_hc==indxGrts, 0]
-        compy = y[y_hc==indxGrts, 1]
+        # compx = x[x_hc==indxGrts, 0]
+        # compy = x[x_hc==indxGrts, 1]
 
+        # for g in range(0, len(self.scoreUrl)):
+        #     #score = self.align(self.entryUrl,self.url[g])
+        #     for i in range(grtst):
+        #         if(self.scoreUrl[g][0]==compx[i] and self.scoreUrl[g][1]==compy[i]):
+        #             print('path', self.url[g], 'score', self.scoreUrl[g])
+        #             break
+
+        compx0 = x[x_hc==0, 0]
+        compy0 = x[x_hc==0, 1]
+        print('cluster 1')
+        print(len(x[x_hc==0, 0]))
         for g in range(0, len(self.scoreUrl)):
-            score = self.align(comp,self.url[g])
-            for i in range(grtst):
-                if(score[0]==compx[i] and score[1]==compy[i]):
-                    print('path', self.url[g], 'score', self.scoreUrl[g])
+            #score = self.align(self.entryUrl,self.url[g])
+            for i in range(len(x[x_hc==0, 0])):
+                if(self.scoreUrl[g][0]==compx0[i] and self.scoreUrl[g][1]==compy0[i]):
+                    print('path', self.url[g], 'score', self.scoreUrl[g],'cluster 1')
+                    break
+
+        compx1 = x[x_hc==1, 0]
+        compy1 = x[x_hc==1, 1]
+        print('cluster 2')
+        print(len(x[x_hc==1, 0]))
+        for g in range(0, len(self.scoreUrl)):
+            #score = self.align(self.entryUrl,self.url[g])
+            for i in range(len(x[x_hc==1, 0])):
+                if(self.scoreUrl[g][0]==compx1[i] and self.scoreUrl[g][1]==compy1[i]):
+                    print('path', self.url[g], 'score', self.scoreUrl[g], 'cluster 2')
+                    break
+
+        compx2 = x[x_hc==2, 0]
+        compy2 = x[x_hc==2, 1]
+        print('cluster 3')
+        print(len(x[x_hc==2, 0]))
+        for g in range(0, len(self.scoreUrl)):
+            #score = self.align(self.entryUrl,self.url[g])
+            for i in range(len(x[x_hc==2, 0])):
+                if(self.scoreUrl[g][0]==compx2[i] and self.scoreUrl[g][1]==compy2[i]):
+                    print('path', self.url[g], 'score', self.scoreUrl[g], 'cluster 3')
+                    break
+
+        compx3 = x[x_hc==3, 0]
+        compy3 = x[x_hc==3, 1]
+        print('cluster 4')
+        print(len(x[x_hc==3, 0]))
+        for g in range(0, len(self.scoreUrl)):
+            #score = self.align(self.entryUrl,self.url[g])
+            for i in range(len(x[x_hc==3, 0])):
+                if(self.scoreUrl[g][0]==compx3[i] and self.scoreUrl[g][1]==compy3[i]):
+                    print('path', self.url[g], 'score', self.scoreUrl[g], 'cluster 4')
+                    break
+
+        # compx4 = x[x_hc==4, 0]
+        # compy4 = x[x_hc==4, 1]
+        # print('cluster 5')
+        # print(len(x[x_hc==4, 0]))
+        # for g in range(0, len(self.scoreUrl)):
+        #     #score = self.align(self.entryUrl,self.url[g])
+        #     for i in range(len(x[x_hc==4, 0])):
+        #         if(self.scoreUrl[g][0]==compx4[i] and self.scoreUrl[g][1]==compy4[i]):
+        #             print('path', self.url[g], 'score', self.scoreUrl[g], 'cluster 5')
+        #             break
         
         print('oi')
+        print(len(x[x_hc==0, 0]))
+        print(len(x[x_hc==1, 0]))
+        print(len(x[x_hc==2, 0]))
+        print(len(x[x_hc==3, 0]))
+        # print(len(x[x_hc==4, 0]))
         self.parsed()
         #spider.logger.info('Spider closed: %s', spider.name)
 
     def parse(self, response):
         noExcepted = []
-        entryUrl = 'https://www.nerdstickers.com.br/'
+        #entryUrl = 'https://www.nerdstickers.com.br/'
         self.url.append(response.url)
-        score = self.align(entryUrl,response.url)
+        score = self.align(self.entryUrl,response.url)
         self.scoreUrl.append(score)
-        links = response.xpath('//@href').extract()
-        for link in links:
-            for word in range(0, len(self.keywords)):
-                noExcepted = re.findall(self.keywords[word], link)
-                if(len(noExcepted)>0):
-                    break
-            if(len(noExcepted)==0):
-                # self.url.append(link)
-                # score = self.align(entryUrl,link)
-                # self.scoreUrl.append(score)
-                yield scrapy.Request(response.urljoin(link), callback=self.parse)
-            #if(response.meta['depth'] < 4):
-                #yie3ld scrapy.Request(response.urljoin(link), callback=self.parse)
+        if(score[1]>=10):
+            links = response.xpath('//@href').extract()
+            for link in links:
+                for word in range(0, len(self.keywords)):
+                    noExcepted = re.findall(self.keywords[word], link)
+                    if(len(noExcepted)>0):
+                        break
+                if(len(noExcepted)==0):
+                    # self.url.append(link)
+                    # score = self.align(entryUrl,link)
+                    # self.scoreUrl.append(score)
+                    yield scrapy.Request(response.urljoin(link), callback=self.parse)
+                #if(response.meta['depth'] < 4):
+                    #yie3ld scrapy.Request(response.urljoin(link), callback=self.parse)
     
     def parsed(self):
         item = EcrawlerItem()
